fce1c66d23a9dec85a1e186051653fc99ec2459f
cesarWrites <cesarwrites24@gmail.com>
2024-02-03 02:30:54 +0300

    modify navbar compoet

--- src/components/Sidenavbar.js
+++ src/components/Sidenavbar.js
@@ -1,68 +1,63 @@
-import React, { useState } from 'react';
-import { Drawer, List, ListItem, ListItemText, IconButton, makeStyles } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import QuickAction from './QuickAction';
+import React from 'react';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, makeStyles } from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home';
+import PeopleIcon from '@material-ui/icons/People';
+import WorkIcon from '@material-ui/icons/Work';
+import NavSection from './NavComponent';
+import logo from '../assets/oca_logo.svg';
+
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   drawer: {
     width: drawerWidth,
-    flexShrink: 0,
+    flexShrink: 0
   },
   drawerPaper: {
     width: drawerWidth,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
+    left: 0,
+    backgroundColor: '#152238',
+    color: '#fff'
   },
   toolbar: theme.mixins.toolbar,
 }));
 
 const SideNavbar = () => {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
-
-  const handleToggleDrawer = () => {
-    setOpen(!open);
-  };
 
   return (
-    <div className='nav-main'>
-      <div className='nav-list'>
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        edge="start"
-        onClick={handleToggleDrawer}
-        className={classes.menuButton}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Drawer
-        className={classes.drawer}
-        variant="temporary"
-        anchor="left"
-        open={open}
-        onClose={handleToggleDrawer}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.toolbar} />
-        <List>
-          <ListItem button>
-            <ListItemText primary="Item 1" />
-          </ListItem>
-          <ListItem button>
-            <ListItemText primary="Item 2" />
-          </ListItem>
-          <ListItem button>
-            <ListItemText primary="Item 3" />
-          </ListItem>
-          {/* Add more items as needed */}
-        </List>
-      </Drawer>
-      </div>
+    <div className='side-nav'>
+    <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+       <img src={logo} alt="OCA logo"/>
+      <div className={classes.toolbar} />
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <PeopleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Users" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <WorkIcon />
+          </ListItemIcon>
+          <ListItemText primary="Projects" />
+        </ListItem>
+        {/* Add more items as needed */}
+      </List>
+    </Drawer>
     </div>
   );
 };
