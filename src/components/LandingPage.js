6036c6e8faac2e615db55b1096df7b93ebd80e0b
cesarWrites <cesarwrites24@gmail.com>
2024-02-05 10:55:07 +0300

    modify navbar componet

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -9,17 +9,12 @@ const LandingPage = () => {
     return (
         <div className='main-dashboard' style={{ height: '100vh'}}>
         <SideNavbar />
-  
-        {/* Main Content Container */}
-        <div style={{ flex: 1, padding: '20px', display: 'flex', flexDirection: 'column', position: 'relative', zIndex: 1 }}>
+        <div style={{ marginLeft: '15%', flex: 1, padding: '20px', display: 'flex', flexDirection: 'column', position: 'relative', zIndex: 1 }}>
           {/* Tables Container */}
           <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-            {/* Table 1 (Side by Side) */}
             <div style={{ flex: 1, marginRight: '20px', zIndex: 0 }}>
               <QuickAction />
             </div>
-  
-            {/* Table 2 (Side by Side) */}
             <div style={{ flex: 1, zIndex: 0 }}>
               <ConfirmedProjects />
             </div>
