b2e0df20cfad402fef624c87b30785302fedd15a
cesarWrites <cesarwrites24@gmail.com>
2024-02-14 05:40:27 +0300

    modify landing page design

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,24 +7,21 @@ import '../App.css';
 
 const LandingPage = () => {
     return (
-        <div className='main-dashboard' style={{ height: '100vh'}}>
+        <div className='dashboard-container'>
+        <div className="sidebar">
         <SideNavbar />
-        <div style={{ marginLeft: '15%', flex: 1, padding: '20px', display: 'flex', flexDirection: 'column', position: 'relative', zIndex: 1 }}>
-          {/* Tables Container */}
-          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-            <div style={{ flex: 1, marginRight: '20px', zIndex: 0 }}>
+        </div>
+        <div className='content'>
+            <div className='quick-links'>
               <QuickAction />
             </div>
-            <div style={{ flex: 1, zIndex: 0 }}>
+            <div className='table-container'>
               <ConfirmedProjects />
             </div>
           </div>
-  
-          {/* Table 3 (At the Bottom) */}
-          <div style={{ marginTop: '20px', zIndex: 0 }}>
+          <div className='table-container'>
             <ProjectList />
           </div>
-        </div>
       </div>
           );
         };
