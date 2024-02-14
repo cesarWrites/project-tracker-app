e344f200576fd546b3e6de22b75857c4d7fce0a1
cesarWrites <cesarwrites24@gmail.com>
2024-02-14 05:51:14 +0300

    modify project approval component

---
+++ src/components/finance/ApproveProject.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProjectReview from './ProjectReview';
+const FinanceApproval = () => {
+  return (
+    <div>
+      <h2>Finance Approval Section</h2>
+      <ProjectReview/>
+    </div>
+  );
+};
+
+export default FinanceApproval;
