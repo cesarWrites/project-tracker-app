5a37d5375cd7f3efd2cd6d5516d5f2cb5b51377a
cesarWrites <cesarwrites24@gmail.com>
2024-02-14 05:50:26 +0300

    create navigation for admin

--- src/components/admin/AdminNav.js
+++ src/components/admin/AdminNav.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
-import CreateUser from './CreateUser';
-import UserTable from './UserList';
+import CreateUser from './CreateUser'; // Import your users table component
+import UserTable from './UserList'; // Import your create user form component
+import { Button } from '@material-ui/core';
 
 const AdminNavigation = () => {
-    return(
-        <div className='admin-nav'>
-            <CreateUser/>
-            <UserTable/>
-        </div>
-    )
-}
+  const [showTable, setShowTable] = useState(true); // State to toggle between table and form
+
+  const toggleView = () => {
+    setShowTable(!showTable); // Toggle the state when the button is clicked
+  };
+
+  return (
+    <div>
+      <Button variant="contained" color="primary" onClick={toggleView}>
+        {showTable ? 'Show Form' : 'Show Table'}
+      </Button>
+      {showTable ? <UserTable /> : <CreateUser />}
+    </div>
+  );
+};
 
 export default AdminNavigation;
